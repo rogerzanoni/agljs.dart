97f0f4d0b3c63796b112e44307d342f0c5e0a9f7
Roger Zanoni <rzanoni@igalia.com>
2021-07-02 14:14:39 +0200

    Add base implementation of runnables promise and AfmApp class

--- lib/agljs.dart
+++ lib/agljs.dart
@@ -1,12 +1,16 @@
 library AGLJS;
 
 import 'package:js/js.dart';
+import 'package:js/js_util.dart';
+
+import 'dart:async';
 
 @JS('AGLJS')
 external AglJs AGLJS;
 
 @JS()
 abstract class AglJs {
+    external AfmMain get afmMain;
     external Api get api;
     external HomeScreen get homescreen;
 }
@@ -19,4 +23,18 @@ abstract class Api {
 @JS()
 abstract class HomeScreen {
     external Object showWindow(String id);
+}
+
+@JS()
+abstract class AfmMain {
+    // TODO: add documentation on how
+    // to use this (runnables() return
+    // a Promise that should be converted to
+    // Future<List<AfmApp>>
+    external dynamic runnables();
+}
+
+@JS()
+abstract class AfmApp {
+    external String get id;
 }
